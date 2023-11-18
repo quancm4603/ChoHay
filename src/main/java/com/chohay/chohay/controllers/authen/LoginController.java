2c218e94d501efa96a34eb7db15dd40eec1b0be3
quancm4603 <135806026+quancm4603@users.noreply.github.com>
2023-11-18 13:47:19 +0700

    add remember cookie

---
+++ src/main/java/com/chohay/chohay/controllers/authen/LoginController.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package com.chohay.chohay.controllers.authen;
+
+import com.chohay.chohay.models.User;
+import com.chohay.chohay.services.UserService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author caomi
+ */
+@WebServlet(name="LoginController", urlPatterns={"/login", "/dang-nhap"})
+public class LoginController extends HttpServlet {
+ 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("user") != null) {
+            response.sendRedirect("./");
+        }else{
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/authen/login.jsp");
+            requestDispatcher.forward(request, response);
+        }
+        
+    } 
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        try {
+            //Get login params
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            
+            //Valitade
+            UserService userService = new UserService();
+            if (userService.validateUser(username, password)) {
+                HttpSession session = request.getSession();
+                User user = userService.getUserByUserNameOrEmail(username);
+                session.setAttribute("user", user);
+                
+                String remember = request.getParameter("remember");
+                if (remember != null) {
+                    Cookie usernameCookie = new Cookie("username", username);
+                    usernameCookie.setMaxAge(60*24*3);
+                    Cookie passwordCookie = new Cookie("password", password);
+                    passwordCookie.setMaxAge(60*24*3);
+                    response.addCookie(usernameCookie);
+                    response.addCookie(passwordCookie);
+                    
+                }
+                
+                response.sendRedirect("./");
+            }else{
+                response.sendRedirect("./login");
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+
+
+}
--- src/test/java/TestServices/TestProductService.java
+++ src/test/java/TestServices/TestProductService.java
@@ -31,36 +31,8 @@ public class TestProductService {
         
     }
     
-    @Test
-    public void testAddProduct()throws SQLException{
-        Product product = new Product();
-        product.setUserId(7);
-        product.setName("Exciter 155000");
-        product.setDescription("Nguyen zin");
-        product.setPrice(50000000);
-        product.setImage("/images/products/exciter.png");
-        product.setPhone("0123456789");
-        //create new address
-        Address address = new Address();
-        address.setProvince("Binh DInh");
-        address.setCity("Quy Nhon");
-        address.setDistrict("Nhon Binh");
-        address.setStreet("Phuong 5");
-        AddressService addressService = new AddressService();
-        //add new address to db
-        int addressId = addressService.addAddress(address);
-        
-        product.setAddressId(addressId);
-        product.setCategory("Vehicle");
-        
-        Details details = new VehicleDetails("Sport", 2020);
-        product.setDetails(details);
-        
-        //add product
-        System.out.println(productService.addProduct(product));
-        
-        
-    }
+
+    
     
     
 }
